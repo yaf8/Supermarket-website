ffc3a98b18c9fedc7468e868542be26dbc9c28b2
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-04-23 22:28:08 +0300

    add injera image and update database

--- database/super_market.sql
+++ database/super_market.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 07:04 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Apr 23, 2023 at 09:26 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,16 +34,26 @@ CREATE TABLE `product` (
   `description` varchar(30) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `img_uri` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `name`, `category`, `description`, `price`, `img_uri`) VALUES
-(3, 'HP envy x360', 'laptop', 'Display size 15.60-inch. \r\nDis', 58996.55, 'hp_envy_x360.jpg'),
-(5, 'Samsung S23 ultra', 'phone', '6.8” Edge QHD+ Dynamic AMOLED ', 120000.00, 'Samsung-Galaxy-S23-Ultra.jpg'),
-(7, 'Lifebuoy ', 'soup', 'Lifebuoy Total 10 Germ Protect', 45.50, 'Lifebuoy_soup.png');
+(3, 'HP envy x360', 'laptop', 'Display size 15.60-inch. \r\nDis', '58996.55', 'hp_envy_x360.jpg'),
+(5, 'Samsung S23 ultra', 'phone', '6.8” Edge QHD+ Dynamic AMOLED ', '120000.00', 'Samsung-Galaxy-S23-Ultra.jpg'),
+(7, 'Lifebuoy ', 'soup', 'Lifebuoy Total 10 Germ Protect', '45.50', 'Lifebuoy_soup.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `top_products`
+--
+
+CREATE TABLE `top_products` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
