06986e34fb801af2afa17cba2c944e10a44cdb5c
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-04-24 22:46:41 +0300

    update database

--- database/super_market.sql
+++ database/super_market.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 09:26 PM
+-- Generation Time: Apr 24, 2023 at 09:45 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -24,6 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `daily`
+--
+
+CREATE TABLE `daily` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discount`
+--
+
+CREATE TABLE `discount` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `new_arrivals`
+--
+
+CREATE TABLE `new_arrivals` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -31,19 +61,22 @@ CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `category` varchar(30) NOT NULL,
-  `description` varchar(30) NOT NULL,
+  `description` varchar(40) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `img_uri` varchar(255) NOT NULL
+  `img_uri` varchar(255) NOT NULL,
+  `subcategory` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `category`, `description`, `price`, `img_uri`) VALUES
-(3, 'HP envy x360', 'laptop', 'Display size 15.60-inch. \r\nDis', '58996.55', 'hp_envy_x360.jpg'),
-(5, 'Samsung S23 ultra', 'phone', '6.8” Edge QHD+ Dynamic AMOLED ', '120000.00', 'Samsung-Galaxy-S23-Ultra.jpg'),
-(7, 'Lifebuoy ', 'soup', 'Lifebuoy Total 10 Germ Protect', '45.50', 'Lifebuoy_soup.png');
+INSERT INTO `product` (`id`, `name`, `category`, `description`, `price`, `img_uri`, `subcategory`) VALUES
+(3, 'HP envy x360', 'electronics', 'Display size 15.60-inch. \r\nDis', '58996.55', 'hp_envy_x360.jpg', 'laptop'),
+(5, 'Samsung S23 ultra', 'electronics', '6.8” Edge QHD+ Dynamic AMOLED ', '120000.00', 'Samsung-Galaxy-S23-Ultra.jpg', 'phone'),
+(7, 'Lifebuoy ', 'sanitary', 'Lifebuoy Total 10 Germ Protect', '45.50', 'Lifebuoy_soup.png', 'soup'),
+(9, 'Banana', 'foods', 'Banana 1KG organic', '44.99', 'banana.jpg', 'cultural'),
+(10, 'Samsung S10', 'electronics', 'Samsung 64GB ROM, 6GB RAM', '50245.00', 'samsung-galaxy-s10-phone.jpeg', 'laptop');
 
 -- --------------------------------------------------------
 
@@ -58,6 +91,24 @@ CREATE TABLE `top_products` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `daily`
+--
+ALTER TABLE `daily`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `discount`
+--
+ALTER TABLE `discount`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `new_arrivals`
+--
+ALTER TABLE `new_arrivals`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product`
@@ -76,7 +127,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
